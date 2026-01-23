32b251d29385981ddca783831f76f620f2b1e4a2
zestymachine <zestymachinemaximus@gmail.com>
2026-01-23 03:54:06 -0800

    attempted to implement double jump

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -9,11 +9,16 @@ public class newScript : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 5.0f; 
+    [SerializeField] float jumpForce = 25f;
+    int jumpCount = 0; 
+    [SerializeField] int maxJumps = 2; 
     Rigidbody2D rb; 
     bool isGrounded = false;
     int score = 0; 
     Animator animator; 
     SpriteRenderer spriteRenderer; 
+    bool jumpPressed = false;
+    
 
     void Start()
     {
@@ -22,13 +27,11 @@ public class newScript : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>(); 
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
     void OnMove(InputValue value) { 
         //first part of vector 2 is value you're moving it to 
         //+1 is to right, -1 to left
@@ -37,7 +40,14 @@ public class newScript : MonoBehaviour
         movementY = v.y; 
         Debug.Log("Movement X = " + movementX);
         Debug.Log("Movement Y = " + movementY);
-        //want to be able to make distance every tick 
+        
+
+    }
+
+    void OnJump(InputValue value) { 
+        if (value.isPressed) { 
+            jumpPressed = true; 
+        }
     }
     //moves according to tick assigned in serialized field 
     void FixedUpdate() { 
@@ -49,14 +59,28 @@ public class newScript : MonoBehaviour
         rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
 
         //jumping code 
-        if (movementY > 0 && isGrounded)
-        { 
-            rb.AddForce(new Vector2(0,100)); 
-            animator.SetBool("isJumping", true); 
+      
+        // if (movementY >0 && isGrounded) 
+        // { 
+        //     rb.AddForce(new Vector2(0,100)); 
+        //     jumpCount+=1; 
+        //     animator.SetBool("isJumping", true); 
+        
+        // }
+        // jumping code (FixedUpdate)
+        if (jumpPressed && jumpCount < maxJumps){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            jumpCount++;
+            animator.SetBool("isJumping", true);
         }
-        else {
-            animator.SetBool("isJumping", false);
-        }
+
+        // prevent repeated jumps while holding input
+        jumpPressed = false; 
+        // else {
+        //     animator.SetBool("isJumping", false);
+        // }
 
         //is movement X close to 0? close to 0 -> player is not moving 
         //if movement x is not 0, set isRunning bool to true 
@@ -79,6 +103,8 @@ public class newScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) { 
         if (collision.gameObject.CompareTag("Ground")) { 
             isGrounded = true; 
+            //reset jumps 
+            jumpCount = 0;
         }
     }
 
